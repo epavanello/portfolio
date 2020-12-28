eeb92bcf88c894604c9d9bef336a2cda39fe23c6
Emanuele Pavanello <e.pavanello@teamsystem.com>
2020-12-29 00:10:26 +0100

    netlify integration

--- svelte.config.js
+++ svelte.config.js
@@ -9,28 +9,12 @@ module.exports = {
     postcss: {
       plugins: [require("tailwindcss")],
     },
-    babel: {
-      presets: [
-        [
-          "@babel/preset-env",
-          {
-            loose: true,
-            // No need for babel to resolve modules
-            modules: false,
-            targets: {
-              // ! Very important. Target es6+
-              esmodules: true,
-            },
-          },
-        ],
-      ],
-    },
   }),
   kit: {
     // By default, `npm run build` will create a standard Node app.
     // You can create optimized builds for different platforms by
     // specifying a different adapter
-    adapter: "@sveltejs/adapter-node",
+    adapter: "@sveltejs/adapter-netlify",
 
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
